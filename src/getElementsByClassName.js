611f0c8bdf437bc00087ddafdce70c1b11d72a97
enriq018 <enriq018coding@gmail.com>
2017-10-10 13:27:44 -0700

    working on both

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,67 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+// var getElementsByClassName = function(className) {
+//   //inputs: string
+//   //outputs: an array of nodes
+//   //assumptions: remember that nodes are array like object
+//   //edge cases: if there are multiple layers then use recursion
+
+//   //create variable storage
+//   var results = [];
+//   //iterate through nodes
+//   var getChildNodes = function (nodes) {
+//     for (var i = 0; i < nodes.length; i++) {   
+//     //assign element to node
+//       var node = nodes[i];
+//     }
+//     //condition if nodes has child nodes and childnodes has className property
+//     if (node.childNode && node.childNode.className) {
+//       results.push(node);
+//     }
+//   };
+  
+
+//   //send children of node back into function
+//   getChildNodes(node);
+
+//   getChildNodes(document.body.childNodes);
+
+// };
+
+var getElementsByClassNam = function(className) {
+  //inputs: string
+  //outputs: an array of nodes
+  //assumptions: remember that nodes are array like object
+  //edge cases: if there are multiple layers then use recursion
+
+  //create variable storage
+  var results = [];
+  //   debugger;
+  //iterate through nodes
+  var getChildNodes = function (nodes) {
+    for (var i = 0; i < nodes.length; i++) {   
+    //assign element to node
+      var node = nodes[i];
+      console.log(node);
+    }
+    
+    if  (node !== undefined) {
+      //condition if nodes has child nodes and childnodes has className property
+      if (node.childNode && node.childNode.className) {
+        results.push(node);
+  
+      }
+      //     send children of node back into function
+      getChildNodes(node);
+    }
+     
+
+  };
+  
+
+  
+  getChildNodes(document.body.childNodes);
+
+  return results;
 };
